e95c50e1937b4bdd49bed8412d7688aad04c66ba
marco112345 <marco@firstfactory.com>
2025-02-07 13:52:19 -0600

    User creation by signup function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post} from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
@@ -13,8 +14,10 @@ export class AuthController{
     //using @Post decorator
 
     @Post('signup')
-    signup(){
-        return this.authService.signup();
+    //dto has been validated
+    signup(@Body() dto: AuthDto){
+        //console.log({dto})
+        return this.authService.signup(dto);
     }
 
     @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from "@nestjs/common";
 import {User, Bookmark} from '@prisma/client'
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 
 @Injectable({})
 export class AuthService {
+    //Prisma inyection to AuthService
     constructor(private prisma: PrismaService){
-
     }
-
     //Now we are going to add some functionality to the class with login() and signup()
-        
-    
-    signup(){
-            return "I am signed up!!!!";
-        }
+    async signup(dto: AuthDto){
+        //Generate the password hash
+        const hash = await argon.hash(dto.password);
+
+        //Save the new user in the bd
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash
+            }
+        })
+        //Return the saved user
+        delete user.hash;
+        return user;
+    }
     
     signin(){
         return "I am signed in!!!!";

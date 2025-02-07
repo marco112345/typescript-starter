55ece49c6167bed0173adbebac46399e674fed4b
marco112345 <marco@firstfactory.com>
2025-02-07 10:03:01 -0600

    Auth module update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,24 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController{
     //To avoid, instaciate the AuthService class
     //we just goes like this:
     constructor(private authService: AuthService) {}
-
     //This is dependency injection, now the controller can user the business logic of the class
+
+    //Now we create some endpoints
+
+    //using @Post decorator
+
+    @Post('signup')
+    signup(){
+        return this.authService.signup();
+    }
+
+    @Post('signin')
+    signuin(){
+        return this.authService.signuin();
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+
+    //Now we are going to add some functionality to the class with login() and signup()
+        
+    
+    signup(){
+            return "I am signed up!!!!";
+        }
+    
+        signuin(){
+            return "I am signed in!!!!";
+        }
+
+}

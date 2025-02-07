6a50989ed77e0f4330a27c7756fb7e28a24fb3bb
marco112345 <marco@firstfactory.com>
2025-02-07 16:23:06 -0600

    Config module installed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({}), 
+    AuthModule, 
+    UserModule, 
+    BookmarkModule, 
+    PrismaModule],
+
   controllers: [AppController],
   providers: [AppService],
 })
